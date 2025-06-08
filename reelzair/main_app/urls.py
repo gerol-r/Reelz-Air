7c861ee37fdaf70f422f72f80f67ada2d569b8a1
Kier Roman <kierroman@gmail.com>
2025-06-08 18:43:24 -0500

    updated model connection to db and checkout_success form

--- reelzair/main_app/urls.py
+++ reelzair/main_app/urls.py
@@ -4,6 +4,7 @@ from . import views # Import views to connect routes to view functions
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('checkout/', views.checkout, name='checkout'),
     path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('checkout/success', views.checkout_success, name='checkout_success'),
 ]
