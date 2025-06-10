b8877322888969e72d25f2c19cf73fa98b13986a
gerol-r <gerol.revills@gmail.com>
2025-06-10 18:56:59 -0400

    home page complete

--- reelzair/main_app/urls.py
+++ reelzair/main_app/urls.py
@@ -3,7 +3,6 @@ from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('checkout/success', views.checkout_success, name='checkout_success'),
